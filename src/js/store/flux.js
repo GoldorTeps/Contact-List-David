5731dc98bad58f986cd8bf7b1c5e9b601760fecf
Goldorteps <agolfofree@hotmail.com>
2023-09-27 19:17:25 +0200

    casi

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,6 +1,10 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			agenda: [],
+			currentAgendaSlug: "Goldor",
+			contactList: "",
+			contactSelected: {},
 			demo: [
 				{
 					title: "FIRST",
@@ -12,118 +16,93 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			],
-			users: [
-			] 
+			]
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
+			// Get contact function
+			getContacts: async () => {
+				try {
+					const { currentAgendaSlug } = getStore();
+					const response = await fetch(`https://assets.breatheco.de/apis/fake/contact/agenda/${currentAgendaSlug}`);
+					const data = await response.json();
+					setStore({ agenda: data });
+				} catch (error) {
+					console.error("Error getting contacts:", error);
+				}
+			},
+
+			// Add contact function
+			addContact: async (newContact) => {
+				try {
+					const response = await fetch("https://assets.breatheco.de/apis/fake/contact/", {
+						method: "POST",
+						body: JSON.stringify(newContact),
+						headers: {
+							"Content-Type": "application/json"
+						}
+					});
+					const data = await response.json();
+					const store = getStore();
+					setStore({ agenda: [...store.agenda, data] });
+					console.log("Contact has been added:", data);
+				} catch (error) {
+					console.error("Error adding contact:", error);
+				}
+			},
+
+			// update contact function
+			updateContact: async (id, updatedContact) => {
+				try {
+					const response = await fetch(`https://assets.breatheco.de/apis/fake/contact/${id}`, {
+						method: "PUT",
+						body: JSON.stringify(updatedContact),
+						headers: {
+							"Content-Type": "application/json"
+						}
+					});
+					const data = await response.json();
+					const store = getStore();
+					const updatedAgenda = store.agenda.map(contact => {
+						if (contact.id === id) {
+							return { ...contact, ...updatedContact };
+						}
+						return contact;
+					});
+					setStore({ agenda: updatedAgenda });
+					console.log("Contact has been updated:", data);
+				} catch (error) {
+					console.error("Error updating contact:", error);
+				}
+			},
+
+			// Delete contact function
+			deleteContact: async (id) => {
+				try {
+					const response = await fetch(`https://assets.breatheco.de/apis/fake/contact/${id}`, {
+						method: "DELETE"
+					});
+					const data = await response.json();
+					const store = getStore();
+					const updatedAgenda = store.agenda.filter((contact) => contact.id !== id);
+					setStore({ agenda: updatedAgenda });
+					console.log("Contact has been deleted:", data);
+				} catch (error) {
+					console.error("Error deleting contact:", error);
+				}
+			},
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
 			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+				// fetch().then().then(data => setStore({ "foo": data.bar }))
 			},
 			changeColor: (index, color) => {
-				//get the store
 				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
 				const demo = store.demo.map((elm, i) => {
 					if (i === index) elm.background = color;
 					return elm;
 				});
-
-				//reset the global store
 				setStore({ demo: demo });
-			},
-
-			//function to get all the initial contacts from the API
-			getInicialContacts: () => {
-				//get the store
-				const store = getStore();
-
-				fetch(' https://playground.4geeks.com/apis/fake/contact/agenda/Goldor')
-				.then(res => {
-					if (!res.ok) throw Error(res.statusText);
-					return res.json();
-				})
-				.then(response => {
-					setStore({ users: response });
-					})
-				.catch(error => console.error(error));
-				
-			},
-
-			//function to add a new contact
-			addContact: (newContact) => {
-				//get the store
-				const store = getStore();
-				const contact = store.users.concat(newContact);
-			
-				//a fetch to update the contact with the new contact
-				fetch(' https://playground.4geeks.com/apis/fake/contact/', {
-					method: 'POST',
-					body: JSON.stringify(contact),
-					headers:{
-						'Content-Type': 'application/json'
-					}
-				})
-				.then(res => {
-					if (!res.ok) throw Error(res.statusText);
-					return res.json();
-				})
-				.then(response => console.log('Success:', response))
-				.catch(error => console.error(error));
-
-				//reset the global store
-				setStore({ users: contact });
-				
-			},
-
-			//function that allows to update contacts
-			update: (index, newContact) => {
-				const store = getStore();
-			console.log(index, newContact)
-				const contact = store.users.map((c, i) => {
-					if (index == i) {
-						c = newContact
-					}
-					return c
-				});
-
-			console.log("test", contact)
-				setStore({ users: contact });
-			},
-
-			//function that allows to delete an item with to determined index
-			delete: (index) => {
-				//get the store
-				const store = getStore();
-
-				const contact = store.users.filter((c, i) => {
-					return index !== i
-				});
-				console.log(index)
-
-				fetch(` https://playground.4geeks.com/apis/fake/contact/${contact_id}`, {
-					method: 'DELETE',
-					headers:{
-						'Content-Type': 'application/json'
-					}
-				})
-				.then(res => {
-					if (!res.ok) throw Error(res.statusText);
-					return res.json();
-				})
-				.then(response => console.log('Success:', response))
-				.catch(error => console.error(error));
-
-				//reset the global store
-				setStore({ users: contact });
 			}
 		}
 	};
